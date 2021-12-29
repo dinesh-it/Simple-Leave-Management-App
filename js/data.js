e54a6c05b162913adb6813c6cc47aa70306ef540
Dinesh D <dd.dinesh.rajakumar@gmail.com>
2021-12-29 17:27:24 +0530

    client config for live app

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
 // Calendar details
 // Client ID and API key from the Developer Console
-var CLIENT_ID = 'YOUR-GOOGLE-DEVELOPER-API-CLIENT-ID';
-var CONFIG_DOC_ID = 'DOCUMENT-ID-OF-APP-CONFIG';
+var CLIENT_ID = '661067137711-dlotfvbf0jhp86o2v69k0o6eulqst95l.apps.googleusercontent.com';
+var CONFIG_DOC_ID = '1StoENo8hKTRDnXskU6i1CkiKk23dtom8LlHcSjzbnco';
